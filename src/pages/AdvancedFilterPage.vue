4a085b9cf0d5796999fba3c2d4c2aa5079478781
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-11-09 15:15:11 +0100

    feat: add fetchTypes & fetchProjects methods

--- src/pages/AdvancedFilterPage.vue
+++ src/pages/AdvancedFilterPage.vue
@@ -1,16 +1,59 @@
 <script>
 // import MyComponent from "./components/MyComponent.vue";
+import axios from "axios";
 
 export default {
   data() {
     return {
-      title: "Hello world",
+      baseUrl: "http://127.0.0.1:8000/api/",
+      types: [],
     };
   },
 
-  // components: {
-  //   MyComponent,
-  // },
+  /* components: {
+     MyComponent,
+   }, */
+
+  methods: {
+    fetchProjects() {
+      const activeTypes = [];
+
+      this.types.forEach((type) => {
+        if (types.active) activeTypes.push(type.id);
+      });
+
+      axios
+        .post(
+          this.baseUrl + "get-projects-by-filters",
+          { activeTypes },
+          { headers: { "Content-Type": "multipart/from-data" } }
+        )
+        .then((response) => {
+          this.filteredProjects = response.data.data;
+        });
+    },
+
+    fetchTypes() {
+      axios.get(this.baseUrl + "types").then((response) => {
+        this.types = response.data.map((type) => {
+          return {
+            ...type,
+            active: false,
+          };
+        });
+      });
+    },
+
+    toggleType(type) {
+      type.active = !type.active;
+      this.fetchProjects();
+    },
+  },
+
+  created() {
+    this.fetchProjects();
+    this.fetchTypes();
+  },
 };
 </script>
 
